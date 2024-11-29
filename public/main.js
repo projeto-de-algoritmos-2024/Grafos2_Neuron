74db94c997481d41ff52def22e3c89fb5d3bdd72
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-11-29 00:29:22 -0300

    feat(main): add selection as web component

--- public/main.js
+++ public/main.js
@@ -1,35 +1,13 @@
-globalThis.div = /** @type {HTMLDivElement} */ (document.querySelector('.selection'))
+/**
+ * @typedef {import('./selection.js').default} RectangularSelection
+ */
 
-let x1 = 0
-let y1 = 0
-let x2 = 0
-let y2 = 0
+const canvas = /** @type {HTMLDivElement} */ (document.querySelector('.canvas'));
+const selection = /** @type {RectangularSelection} */ (document.createElement('rectangular-selection'));
 
-function update()
-{
-	const x3 = Math.min(x1, x2)
-	const x4 = Math.max(x1, x2)
-	const y3 = Math.min(y1, y2)
-	const y4 = Math.max(y1, y2)
-	window.div.style.left = `${x3}px`
-	window.div.style.top = `${y3}px`
-	window.div.style.width = `${x4 - x3}px`
-	window.div.style.height = `${y4 - y3}px`
-}
+canvas.append(selection)
+selection.hidden = true
 
-window.addEventListener('mousedown', e => {
-	window.div.hidden = false
-	x1 = e.clientX
-	y1 = e.clientY
-	update()
-})
-
-window.addEventListener('mousemove', e => {
-	x2 = e.clientX
-	y2 = e.clientY
-	update()
-})
-
-window.addEventListener('mouseup', () => {
-	div.hidden = true
-})
+canvas.addEventListener('mouseup', () => selection.mouseup())
+canvas.addEventListener('mousedown', e => selection.mousedown(e))
+canvas.addEventListener('mousemove', e => selection.mousemove(e))
---
+++ public/selection.js
@@ -0,0 +1,54 @@
+export default class RectangularSelection extends HTMLElement {
+	x1 = 0
+	y1 = 0
+	x2 = 0
+	y2 = 0
+
+	constructor() {
+		super()
+		this.x1 = 0
+		this.y1 = 0
+		this.x2 = 0
+		this.y2 = 0
+	}
+
+	update()
+	{
+		const x3 = Math.min(this.x1, this.x2)
+		const x4 = Math.max(this.x1, this.x2)
+		const y3 = Math.min(this.y1, this.y2)
+		const y4 = Math.max(this.y1, this.y2)
+		this.style.left = `${x3}px`
+		this.style.top = `${y3}px`
+		this.style.width = `${x4 - x3}px`
+		this.style.height = `${y4 - y3}px`
+	}
+
+	/**
+	 * @param {MouseEvent} e
+	 */
+	mousedown(e)
+	{
+		this.hidden = false
+		this.x1 = e.clientX
+		this.y1 = e.clientY
+		this.update()
+	}
+
+	/**
+	 * @param {MouseEvent} e
+	 */
+	mousemove(e)
+	{
+		this.x2 = e.clientX
+		this.y2 = e.clientY
+		this.update()
+	}
+
+	mouseup()
+	{
+		this.hidden = true
+	}
+}
+
+customElements.define("rectangular-selection", RectangularSelection);
